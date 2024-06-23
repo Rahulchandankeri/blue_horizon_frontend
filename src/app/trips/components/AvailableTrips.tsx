0fc37a05f31f11dbfc6ac74dac2cc9c054aa423b
Rahul Chandankeri <rahulchandankeri777@gmail.com>
2024-06-23 09:07:14 +0530

    Server component issue fix

---
+++ src/app/trips/components/AvailableTrips.tsx
@@ -0,0 +1,43 @@
+'use client';
+import BusCard from '@/components/features/BusCard';
+import tripServices from '@/services/tripServices';
+import { Grid } from '@mui/joy';
+import { useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
+const AvailableTrips = () => {
+  const searchParams = useSearchParams();
+
+  const [trips, setTrips] = useState<any>(null);
+
+  const getTrips = async () => {
+    try {
+      const payload = {
+        source: searchParams.get('source'),
+        destination: searchParams.get('destination'),
+        journeyDate: '22/06/2024',
+      };
+      const response = await tripServices.getAvailableTrips(payload);
+      setTrips(response);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getTrips();
+  }, []);
+
+  return (
+    <>
+      {' '}
+      {trips?.availableTrips?.map((bus: any, index: number) => {
+        return (
+          <Grid xs={12} key={index}>
+            <BusCard bus={bus} />
+          </Grid>
+        );
+      })}
+    </>
+  );
+};
+
+export default AvailableTrips;
--- src/app/trips/page.tsx
+++ src/app/trips/page.tsx
@@ -1,46 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import BusCard from '@/components/features/BusCard';
 import Main from '@/components/layouts/Main';
-import tripServices from '@/services/tripServices';
 import { Grid } from '@mui/joy';
+import { Suspense } from 'react';
+import AvailableTrips from './components/AvailableTrips';
 
-interface Trip {
-  id: number;
-  name: string;
-  // Add other trip fields here
-}
-const fetchTrips = async (searchParams: URLSearchParams): Promise<Trip[]> => {
-  try {
-    const payload = {
-      source: searchParams.get('source'),
-      destination: searchParams.get('destination'),
-      journeyDate: '22/06/2024',
-    };
-    const response = await tripServices.getAvailableTrips(payload);
-    return response?.availableTrips || [];
-  } catch (error) {
-    console.error('Error fetching trips:', error);
-    return [];
-  }
-};
-
-const TripsPage = async ({ searchParams }: { searchParams: URLSearchParams }) => {
-  const trips = await fetchTrips(searchParams);
-
+const TripsPage = () => {
   return (
     <Main>
-      {/* <BusInfoCard busNumber="22B" route="Downtown to Uptown" departureTime="08:30 AM" arrivalTime="09:15 AM" status="On Time" /> */}
-      <Grid container spacing={2}>
-        {trips?.map((bus: any, index: number) => {
-          return (
-            <Grid xs={12} key={index}>
-              <BusCard bus={bus} />
-            </Grid>
-          );
-        })}
-      </Grid>
+      <Suspense>
+        <Grid container spacing={2}>
+          <AvailableTrips />
+        </Grid>
+      </Suspense>
       <script async src="https://checkout.razorpay.com/v1/checkout.js"></script>
     </Main>
   );
 };
+
 export default TripsPage;
